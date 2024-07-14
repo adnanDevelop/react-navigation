7d6b173101039675e1a4f7f95d89604c0847ceb1
Adnan Tariq <adnan6official@gmail.com>
2024-07-14 23:45:44 +0500

    in drawer component i created sidebar using drawer package

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,35 +1,22 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { StyleSheet } from "react-native";
+import "react-native-gesture-handler";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Home from "./Home";
+import TopBar from "./TopBar";
 
-const About = ({ navigation, route }) => {
-  // console.log(route?.params);
+const Drawer = createDrawerNavigator();
+
+const About = () => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>My name is: {route?.params?.name}</Text>
-      <Text>My age is: {route?.params?.age}</Text>
-      <Text>And my qualification is: {route?.params?.qualification}</Text>
-
-      <TouchableOpacity onPress={() => navigation.navigate("Home")}>
-        <Text
-          style={{
-            fontWeight: "bold",
-            width: 120,
-            height: 40,
-            lineHeight: 40,
-            backgroundColor: "blue",
-            color: "white",
-            textAlign: "center",
-            borderRadius: 10,
-            marginTop: 10,
-          }}
-        >
-          Go to Home Screen
-        </Text>
-      </TouchableOpacity>
-    </View>
+    <>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="topbar" component={TopBar} />
+      </Drawer.Navigator>
+    </>
   );
 };
 
 export default About;
-
 const styles = StyleSheet.create({});
