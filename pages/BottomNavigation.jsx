ca9db800af6ce8fd1e4c976374a91b275fe02f60
Adnan Tariq <adnan6official@gmail.com>
2024-07-14 22:04:14 +0500

    in this video i'll learn about bottom tab navigation

---
+++ pages/BottomNavigation.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const BottomNavigation = () => {
+  return (
+    <View>
+      <Text>BottomNavigation</Text>
+    </View>
+  );
+};
+
+export default BottomNavigation;
+
+const styles = StyleSheet.create({});
