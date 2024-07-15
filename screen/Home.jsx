e1c6441138a4047fa9921dd0767d45f40e73192e
Adnan Tariq <adnan6official@gmail.com>
2024-07-15 10:33:43 +0500

    Now this time i used product api and displayed data on frontend using map method

--- screen/Home.jsx
+++ screen/Home.jsx
@@ -1,9 +1,11 @@
 import {
   ActivityIndicator,
+  Image,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
+  ScrollView,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 
@@ -14,9 +16,7 @@ const Home = ({ navigation }) => {
   const fetchData = async () => {
     setLoading(true);
     try {
-      const apiData = await fetch(
-        "https://jsonplaceholder.typicode.com/users/1"
-      );
+      const apiData = await fetch("https://fakestoreapi.com/products/");
       const response = await apiData.json();
       setLoading(false);
       setData(response);
@@ -30,75 +30,88 @@ const Home = ({ navigation }) => {
     fetchData();
   }, []);
 
+  console.log(data);
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      {/* About page button */}
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate("About", {
-            name: "Adnan Tariq",
-            age: 21,
-            qualification: "Matric",
-          })
-        }
-      >
-        <Text
-          style={{
-            fontWeight: "bold",
-            paddingHorizontal: 20,
-            height: 45,
-            lineHeight: 45,
-            backgroundColor: "blue",
-            color: "white",
-            textAlign: "center",
-            borderRadius: 10,
-            marginTop: 10,
-          }}
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        {/* About page button */}
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("About", {
+              name: "Adnan Tariq",
+              age: 21,
+              qualification: "Matric",
+            })
+          }
         >
-          Go to About
-        </Text>
-      </TouchableOpacity>
+          <Text
+            style={{
+              fontWeight: "bold",
+              paddingHorizontal: 20,
+              height: 45,
+              lineHeight: 45,
+              backgroundColor: "blue",
+              color: "white",
+              textAlign: "center",
+              borderRadius: 10,
+              marginTop: 10,
+            }}
+          >
+            Go to About
+          </Text>
+        </TouchableOpacity>
 
-      {/* Bottom navigation page button */}
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate("bottomnavigation", {
-            name: "Adnan Tariq",
-            age: 21,
-            qualification: "Matric",
-          })
-        }
-      >
-        <Text
-          style={{
-            fontWeight: "bold",
-            paddingHorizontal: 20,
-            height: 45,
-            lineHeight: 45,
-            backgroundColor: "blue",
-            color: "white",
-            textAlign: "center",
-            borderRadius: 10,
-            marginTop: 10,
-          }}
+        {/* Bottom navigation page button */}
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("bottomnavigation", {
+              name: "Adnan Tariq",
+              age: 21,
+              qualification: "Matric",
+            })
+          }
         >
-          Go to Tab Navigation
-        </Text>
-      </TouchableOpacity>
+          <Text
+            style={{
+              fontWeight: "bold",
+              paddingHorizontal: 20,
+              height: 45,
+              lineHeight: 45,
+              backgroundColor: "blue",
+              color: "white",
+              textAlign: "center",
+              borderRadius: 10,
+              marginTop: 10,
+            }}
+          >
+            Go to Tab Navigation
+          </Text>
+        </TouchableOpacity>
 
-      {/* calling api */}
-      <View style={{ marginTop: 20 }}>
-        {loading ? (
-          <ActivityIndicator size={50} color="blue" />
-        ) : (
-          <>
-            <Text>Data from api</Text>
-            <Text>Name: {data?.name}</Text>
-            <Text>Email: {data?.email}</Text>
-          </>
-        )}
+        {/* calling api */}
+        <View style={{ marginTop: 20 }}>
+          {loading ? (
+            <ActivityIndicator size={50} color="blue" />
+          ) : (
+            <View style={{ paddingHorizontal: 20 }}>
+              {data?.map((element, index) => {
+                // console.log(element.image);
+                return (
+                  <View key={index} style={{ marginBottom: 15 }}>
+                    <Text>Name: {element?.title}</Text>
+                    <Image
+                      source={{ uri: element?.image }}
+                      style={{ height: 250, objectFit: "cover" }}
+                    />
+                  </View>
+                );
+              })}
+            </View>
+          )}
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
